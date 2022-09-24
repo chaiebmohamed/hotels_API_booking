5242f8a1fdec4b120f4e5bc3b35a13ece028a864
MouhaChaieb <chaiiebmohamed@gmail.com>
2022-09-24 19:56:47 +0200

    make clinet see availble rooms

--- routes/room.js
+++ routes/room.js
@@ -2,10 +2,13 @@ const router = require('express').Router();
 const roomController = require('../controllers/room');
 const auth=require("../middlewares/auth")
 const admin=require("../middlewares/admin")
+const client=require("../middlewares/client")
 const error=require("../middlewares/error")
-const validateObjetId=require("../middlewares/validateObjectId")
+const validateObjetId=require("../middlewares/validateObjectId");
+
 
 router.get("/all",auth,admin,roomController.getAll,error)
+router.get("/allroom",roomController.getEmpltyRooms,error)
 router.post('/addroom',auth,admin,roomController.addRoom,error);
 router.put('/notavailble/:id',validateObjetId,auth,admin,roomController.changeStatusToNotAvailble,error);
 router.put('/availble/:id',validateObjetId,auth,admin,roomController.changeStatusToAvailble,error);
